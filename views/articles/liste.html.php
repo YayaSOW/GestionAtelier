59790107aaa83d7fcb59e95d61ac93481d2ef086
Yaya SOW <yaya.sowzone@gmail.com.com>
2024-06-02 14:43:29 +0000

    V1.0

---
+++ views/articles/liste.html.php
@@ -0,0 +1,101 @@
+<nav class="bg-white shadow-md flex items-center justify-center text-xl font-bold px-4 py-2">
+    <h1>Liste Article</h1>
+</nav>
+<main class="p-4 flex-1 overflow-y-auto">
+    <h3 class="text-xl font-bold my-3">Les Articles</h3>
+    <div class="flex justify-end mb-3">
+        <a id="" class="bg-gray-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded" href=""
+            role="button">Nouveau</a>
+    </div>
+    <div class="bg-white shadow-md rounded-md">
+        <table class="w-full table-fixed border-collapse">
+            <thead>
+                <tr class="bg-gray-200">
+                    <th class="py-2 px-4 w-1/3">Libelle</th>
+                    <th class="py-2 px-4 w-1/5">Qte Stock</th>
+                    <th class="py-2 px-4 w-1/5">Prix</th>
+                    <th class="py-2 px-4 w-1/3">Categorie</th>
+                    <th class="py-2 px-4 w-1/3">Type</th>
+                    <th class="py-2 px-4 w-1/3">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td class="py-2 px-4">
+                        <center>1</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>Mark</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>Otto</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>mdo</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>mdo</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+                            <i class="fa-solid fa-pen-to-square"></i> Edit
+                        </button>
+                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                            <i class="fa-solid fa-trash"></i> Delete
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="py-2 px-4">
+                        <center>1</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>Mark</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>Otto</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>mdo</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>mdo</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+                            <i class="fa-solid fa-pen-to-square"></i> Edit
+                        </button>
+                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                            <i class="fa-solid fa-trash"></i> Delete
+                        </button>
+                    </td>
+                </tr>
+                <tr>
+                    <td class="py-2 px-4">
+                        <center>1</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>Mark</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>Otto</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>mdo</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <center>mdo</center>
+                    </td>
+                    <td class="py-2 px-4">
+                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded">
+                            <i class="fa-solid fa-pen-to-square"></i> Edit
+                        </button>
+                        <button class="bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded">
+                            <i class="fa-solid fa-trash"></i> Delete
+                        </button>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+</main>
---
+++ views/categories/liste.html.php
@@ -0,0 +1,40 @@
+v
+<div class="container mb-5">
+    <div class="max-w-md mx-auto bg-white shadow-md rounded-md mt-5">
+        <div class="bg-gray-100 text-gray-800 font-bold p-2">Liste des Categories d'Articles</div>
+        <div class="p-4">
+            <form>
+                <div class="mb-4">
+                    <label for="NomType" class="block text-sm font-bold mb-2">Nom Categorie</label>
+                    <input name="nomType" type="text"
+                        class="block w-full p-2 pl-10 text-sm text-gray-700 placeholder-gray-400 border border-gray-300 rounded-md focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out"
+                        id="NomType" aria-describedby="emailHelp" required>
+                </div>
+                <div class="flex justify-end mb-4">
+                    <button type="submit" class="bg-gray-800 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                        Enregister
+                    </button>
+                </div>
+            </form>
+            <div class="overflow-x-auto">
+                <table class="table-auto w-full border-collapse">
+                    <thead>
+                        <tr class="bg-gray-200">
+                            <th class="py-2 px-4">ID</th>
+                            <th class="py-2 px-4">Nom Categorie</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="border-b">
+                            <td class="py-2 px-4">
+                                <center>Type1</center>
+                            </td>
+                            <td class="py-2 px-4">
+                                <center>Type2</center>
+                            </td>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
